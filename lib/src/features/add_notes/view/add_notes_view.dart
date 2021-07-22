8c5d1cc68db08b972ffb9e8fc626e32ec59af6fc
Arthur Gomes Valadão <arthur.valadao@ITBAM.ORG.BR>
2021-07-22 14:05:01 -0400

    inserção tela add notas

--- lib/src/features/add_notes/view/add_notes_view.dart
+++ lib/src/features/add_notes/view/add_notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_project/src/features/home_page/view/home_page_view.dart';
 
 class AddNotes extends StatelessWidget {
   const AddNotes({Key? key}) : super(key: key);
@@ -7,6 +8,57 @@ class AddNotes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String entrada = '';
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Center(child: Text("Adicionar Nota")),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Title:',
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: SingleChildScrollView(
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      labelText: 'Text',
+                    ),
+                    onChanged: (value) {
+                      entrada = value;
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(entrada);
+                  },
+                  child: Text("Salvar"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/src/features/home_page/view/home_page_view.dart
+++ lib/src/features/home_page/view/home_page_view.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:notes_project/src/features/add_notes/view/add_notes_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   static const route = "homePage";
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> vetor = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,22 +22,38 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: EdgeInsets.only(left: 40, top: 15, right: 40, bottom: 10),
-        child: Card(
-            color: Colors.amberAccent,
-            child: Container(
-              height: 70,
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text("Notas exemplo"),
-              ),
-            )),
+        child: ListView.builder(
+            itemCount: vetor.length,
+            itemBuilder: (_, index) {
+              return Card(
+                  color: Colors.amberAccent,
+                  child: Container(
+                    height: 70,
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(vetor[index]),
+                    ),
+                  ));
+            }),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        backgroundColor: Colors.blue,
-        onPressed: () {},
+        backgroundColor: Colors.amber,
+        onPressed: () async {
+          final word =
+              await Navigator.of(context).push(MaterialPageRoute<String>(
+            builder: (BuildContext context) => AddNotes(),
+            fullscreenDialog: true,
+          ));
+
+          if (word != null) {
+            setState(() {
+              vetor.add(word);
+            });
+          }
+        },
       ),
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
